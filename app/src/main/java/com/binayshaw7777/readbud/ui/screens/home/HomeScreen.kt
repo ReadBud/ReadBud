db6b3f774d70d23de2f91f893ec744f7e56b4091
Binay Shaw <binayshaw7777@gmail.com>
2023-06-27 03:03:06 +0530

    Added FAB

--- app/src/main/java/com/binayshaw7777/readbud/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/binayshaw7777/readbud/ui/screens/home/HomeScreen.kt
@@ -9,13 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
@@ -29,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.binayshaw7777.readbud.ui.theme.PrimaryContainer
 
 
 @Preview(showBackground = true)
@@ -36,9 +41,11 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun HomeScreen() {
     Column(
-        Modifier
+        modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
     ) {
 
         var searchQuery by rememberSaveable { mutableStateOf("") }
@@ -56,6 +63,21 @@ fun HomeScreen() {
             placeholder = { Text("Search your last scan") },
             leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) }
         ) {
+        }
+
+        FloatingActionButton(
+            modifier = Modifier.align(Alignment.End).padding(20.dp),
+            onClick = {
+                //OnClick Method
+            },
+            containerColor = PrimaryContainer,
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Add,
+                contentDescription = "Add FAB",
+                tint = Color.White,
+            )
         }
     }
 }
