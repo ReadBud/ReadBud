967345ed679140734333349a75247058210fe4fb
Binay <binayshaw7777@gmail.com>
2023-07-22 08:49:23 +0530

    TTS Imp completed

--- app/src/main/java/com/binayshaw7777/readbud/components/SettingsListItem.kt
+++ app/src/main/java/com/binayshaw7777/readbud/components/SettingsListItem.kt
@@ -1,6 +1,7 @@
 package com.binayshaw7777.readbud.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -27,7 +29,10 @@ fun SettingsListItem(
         modifier = Modifier
             .fillMaxWidth()
             .height(50.dp)
-            .clickable { onClickSettingsItem() },
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) { onClickSettingsItem() },
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
