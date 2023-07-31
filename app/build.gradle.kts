ac638b4f7107e580aece4748014af2fb9b05f88c
Binay <binayshaw7777@gmail.com>
2023-07-31 11:37:49 +0530

    Upgraded version name and code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.binayshaw7777.readbud"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
